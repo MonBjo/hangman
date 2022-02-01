bf8223d430c592a235d1625ca6aa5c3edee9a784
Lilyklingberg <zhang.lei92@outlook.com>
2022-02-01 16:05:37 +0100

    Fix letter box and style

--- index.js
+++ index.js
@@ -9,17 +9,17 @@
 
  x Ta fram ord ur listan med ord
  x Användaren trycker på en tangent
- xkolla så att det faktiskt är en bokstav
+ x kolla så att det faktiskt är en bokstav
  x kolla så att bokstaven är ny
 
  x jämnför bokstaven med ordet
  x om fel bokstav visa den under 
-    xfyll i ett del på gubben
+    x fyll i ett del på gubben
  x om rätt fyll i bokstav i ordet
     x i rätt ordning
 
- xnär man vinner visa "du vann"-skärmen + fråga om att spela igen
- xnär man förlorar visa "du förlorade"-skärmen + fråga om att spela igen
+ x när man vinner visa "du vann"-skärmen + fråga om att spela igen
+ x när man förlorar visa "du förlorade"-skärmen + fråga om att spela igen
 
  */
 
@@ -38,18 +38,18 @@
  function generateWord(){
     wordToGuess = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)].toLowerCase();
     console.log(`wordToGuess: ${wordToGuess}`);
+    for(let char of wordToGuess){
+        wordGuessByPlayer.push('')
+    }
+    console.log('test: ', wordGuessByPlayer)
  }
  generateWord();
 displayLetterBox()
  document.addEventListener('keydown', (event) => {
     const char = event.key.toLowerCase();
-     
     console.log(char);
-    console.log('char.length === 1 && char.match(/[a-z]/i) ', char.length === 1 && char.match(/[a-z]/i));
-
     if(!hasKeyBeenPressedBefore(char) && checkLetter(char)){
         compare(char, wordToGuess);
-        //console.log('isTrue: ', isTrue);
         displayLetter(isTrue, char);
         showHangMan();
         playerWin();
@@ -61,50 +61,41 @@ displayLetterBox()
     for(let letter of guessedLetters){
         if(char == letter) {
             return true;
-        }
-        //console.log('char', char);
-        //console.log('letter', letter);
+        };
     }
  }
  
  
  function compare(char, wordToGuess) {
     isTrue = false;
-    //console.log(`wordToGuess.length: ${wordToGuess.length}`);
+    console.log(`wordToGuess.length: ${wordToGuess.length}`);
     for(let i = 0; i < wordToGuess.length; i++){
         if (char == wordToGuess.charAt(i)){
-            console.log("rätt!");
-            //wordGuessByPlayer.slice(char, i);
             wordGuessByPlayer[i] = char;
-            //console.log(wordGuessByPlayer);
             isTrue = true;
-            
-        } else {
-            console.log("fel!");
-            //console.log(i + ' char ' + char);
-            //console.log(i + ' word ' + wordToGuess.charAt(i));
-            //console.log(i + wordToGuess);
-            
-        }
+        }else {
+            console.log("fel!");   
+                    
+        } 
     }
+    console.log('wordGuessBy: ', wordGuessByPlayer)
     guessedLetters.push(char);
-     
  }
  
  
  function displayLetter(guessedLetter, char){
     if(isTrue) {
         /* visa i .wordGuess */
-        //wordGuessElem.innerHTML = "Rätt: ";
+        wordGuessElem.innerHTML = "Rätt: ";
         for(let char of wordGuessByPlayer){
-            for(let letterBox of boxes){
+            console.log('wordGuess: ', wordGuessByPlayer)
                 if(char != undefined){
-                    letterBox.innerHTML = char;
-                    console.log(letterBox)
+                    wordGuessElem.innerHTML += `<span>` + char + `</span>`;
                     console.log(wordGuessElem)
-                    break;
+                }else{
+                    wordGuessElem.innerHTML+=`<span></span>`;
                 }
-            }
+            
         }
     } else {
         /* visa i .guessedLetters */
@@ -137,7 +128,6 @@ displayLetterBox()
         default:
             console.log('What are you doing?')
      }
-     console.log('Funka', guessedLettersElem.innerHTML.length)
  }
  
  
@@ -146,16 +136,12 @@ displayLetterBox()
     let word = '';
     for(let letter of wordGuessByPlayer){
         word += letter;
-        //console.log('gusseWord: ', word)
-        //console.log('Letter: ', letter)
     }
     if(word == wordToGuess){
-        console.log('You win');
         winGame.classList.add('show');
     }
  }
  function playerLose(){  
-    console.log('you lose');
     document.querySelector('.right-word').innerHTML = wordToGuess;
     loseGame.classList.add('show');
  }
@@ -177,12 +163,9 @@ displayLetterBox()
     }
 }
 
-function displayLetterBox(){
-    
+function displayLetterBox(){  
     for(let letter in wordToGuess){
-        let letterBox = document.createElement('div')
-        boxes.push(letterBox)
+        let letterBox = document.createElement('span')
         wordGuessElem.appendChild(letterBox)
     }
-    console.log(boxes)
 }
