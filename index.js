060cbbc15acb8a1034d17262754d410ed1659b51
Lilyklingberg <zhang.lei92@outlook.com>
2022-02-01 14:42:19 +0100

    Add display letter boxes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
  x Ta fram ord ur listan med ord
  x Användaren trycker på en tangent
- kolla så att det faktiskt är en bokstav
+ xkolla så att det faktiskt är en bokstav
  x kolla så att bokstaven är ny
 
  x jämnför bokstaven med ordet
@@ -31,6 +31,7 @@
  let wordsToGuess = ['katt', 'hund', 'chinchilla', 'anka', 'Kanin'];
  let guessedLetters = [];
  let wordGuessByPlayer = [];
+ let boxes =[];
  let isTrue;
  let wordToGuess;
 
@@ -39,7 +40,7 @@
     console.log(`wordToGuess: ${wordToGuess}`);
  }
  generateWord();
-
+displayLetterBox()
  document.addEventListener('keydown', (event) => {
     const char = event.key.toLowerCase();
      
@@ -94,10 +95,15 @@
  function displayLetter(guessedLetter, char){
     if(isTrue) {
         /* visa i .wordGuess */
-        wordGuessElem.innerHTML = "Rätt: ";
+        //wordGuessElem.innerHTML = "Rätt: ";
         for(let char of wordGuessByPlayer){
-            if(char != undefined){
-                wordGuessElem.innerHTML += char;
+            for(let letterBox of boxes){
+                if(char != undefined){
+                    letterBox.innerHTML = char;
+                    console.log(letterBox)
+                    console.log(wordGuessElem)
+                    break;
+                }
             }
         }
     } else {
@@ -169,4 +175,14 @@
         // if it's not empty it returns as true
         return char.length === 1 && char.match(/[a-z]/i);
     }
+}
+
+function displayLetterBox(){
+    
+    for(let letter in wordToGuess){
+        let letterBox = document.createElement('div')
+        boxes.push(letterBox)
+        wordGuessElem.appendChild(letterBox)
+    }
+    console.log(boxes)
 }
