c8448bcdbdd0dff4cc11c483c5409d97889c2871
Monica Björk <m.bjork95@gmail.com>
2022-02-02 17:50:26 +0100

    removed some comments & made it so the timer stops upon winning the game

--- index.js
+++ index.js
@@ -1,92 +1,84 @@
-/**
- För att toggla SVG:en
- document.querySelector('figure').classList.add('scaffold')
- document.querySelector('figure').classList.add('head')
- document.querySelector('figure').classList.add('body')
- document.querySelector('figure').classList.add('arms')
- document.querySelector('figure').classList.add('legs')
-
-
- x Ta fram ord ur listan med ord
- x Användaren trycker på en tangent
- x kolla så att det faktiskt är en bokstav
- x kolla så att bokstaven är ny
-
- x jämnför bokstaven med ordet
- x om fel bokstav visa den under 
-    x fyll i ett del på gubben
- x om rätt fyll i bokstav i ordet
-    x i rätt ordning
-
- x när man vinner visa "du vann"-skärmen + fråga om att spela igen
- x när man förlorar visa "du förlorade"-skärmen + fråga om att spela igen
-
- */
-
- let wordGuessElem = document.querySelector('.wordGuess');
- let guessedLettersElem = document.querySelector('.guessedLetters');
- let winGame = document.querySelector('.layout-win')
- let loseGame = document.querySelector('.layout-lose')
+/* defining data */
+let wordGuessElem = document.querySelector('.wordGuess');
+let guessedLettersElem = document.querySelector('.guessedLetters');
+let winGame = document.querySelector('.layout-win');
+let loseGame = document.querySelector('.layout-lose');
+let playAgain = document.getElementsByTagName('a');
  
- let wordsToGuess = ['katt', 'hund', 'chinchilla', 'anka', 'Kanin'];
- let guessedLetters = [];
- let wordGuessByPlayer = [];
- let boxes =[];
- let isTrue;
- let wordToGuess;
- let timeLeft = 60;
+let wordsToGuess = ['katt', 'hund', 'chinchilla', 'anka', 'Kanin'];
+let guessedLetters = [];
+let wordGuessByPlayer = [];
+let boxes =[];
+let toDisplayLetter;
+let wordToGuess;
+let timeLeft = 60;
+let stopTimer = false;
 
- function generateWord(){
+
+/* setup game */
+function generateWord(){
     wordToGuess = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)].toLowerCase();
-    console.log(`wordToGuess: ${wordToGuess}`);
+    //console.log(`wordToGuess: ${wordToGuess}`);
+    
     for(let char of wordToGuess){
         wordGuessByPlayer.push('');
     }
-    console.log('test: ', wordGuessByPlayer);
- }
+}
 
 generateWord();
 displayLetterBox();
-countDownTimer();
+countDownTimer(true);
 
- document.addEventListener('keydown', (event) => {
+
+
+/* add eventListers */
+
+document.addEventListener('keydown', (event) => {
     const char = event.key.toLowerCase();
     //console.log(char);
     if(!hasKeyBeenPressedBefore(char) && checkLetter(char)){
         compare(char, wordToGuess);
-        displayLetter(isTrue, char);
+        displayLetter(toDisplayLetter, char);
         showHangMan();
         playerWin();
     }
- });
- 
+});
 
- function hasKeyBeenPressedBefore(char){
+for(let link of playAgain){
+    link.addEventListener('click', ()=>{
+        window.location.reload();
+    })
+}
+
+
+
+
+function hasKeyBeenPressedBefore(char){
     //console.log('hasKeyBeenPressedBefore:');
     for(let letter of guessedLetters){
         if(char == letter) {
             return true;
         };
     }
- }
+}
  
 
- function compare(char, wordToGuess) {
-    isTrue = false;
+function compare(char, wordToGuess) {
+    toDisplayLetter = false;
     //console.log(`wordToGuess.length: ${wordToGuess.length}`);
     for(let i = 0; i < wordToGuess.length; i++){
         if (char == wordToGuess.charAt(i)){
             wordGuessByPlayer[i] = char;
-            isTrue = true;
+            toDisplayLetter = true;
         }
     }
     //console.log('wordGuessBy: ', wordGuessByPlayer);
     guessedLetters.push(char);
- }
+}
  
  
- function displayLetter(guessedLetter, char){
-    if(isTrue) {
+function displayLetter(guessedLetter, char){
+    if(toDisplayLetter) {
         /* Show in .wordGuess */
         wordGuessElem.innerHTML = ``;
         for(let char of wordGuessByPlayer){
@@ -94,7 +86,7 @@ countDownTimer();
                 if(char != undefined){
                     wordGuessElem.innerHTML += `<span>` + char + `</span>`;
                     //console.log(wordGuessElem);
-                }else{
+                } else{
                     wordGuessElem.innerHTML+=`<span></span>`;
                 }
             
@@ -103,7 +95,9 @@ countDownTimer();
         /* Show in .guessedLetters */
         guessedLettersElem.innerHTML += char;
     }
- }
+}
+
+
 function showHangMan (){
      switch(guessedLettersElem.innerHTML.length){
         case 0:
@@ -130,8 +124,7 @@ function showHangMan (){
             console.log('What are you doing?');
     }
 }
- 
- 
+  
  
 function playerWin(){
     let word = '';
@@ -140,30 +133,27 @@ function playerWin(){
     }
     if(word == wordToGuess){
         winGame.classList.add('show');
+        stopTimer = true;
     }
+
 }
 function playerLose(){  
    document.querySelector('.right-word').innerHTML = wordToGuess;
    loseGame.classList.add('show');
 }
 
-let playAgain = document.getElementsByTagName('a')
-for(let link of playAgain){
-    link.addEventListener('click', ()=>{
-        window.location.reload();
-    })
-}
 
- function checkLetter(char){
+function checkLetter(char){
     if(char == 'å' || char == 'ä' || char == 'ö') {
         return true;
     } else {
-        // if it's empty it returns as 'false'
-        // if it's not empty it returns a character that 
-        // is recieved as true
+        // if char empty it returns as 'false'
+        // if char is not empty it returns the character 
+        // and it is recieved as true
         return char.length === 1 && char.match(/[a-z]/i);
     }
 }
+
 
 function displayLetterBox(){  
     for(let letter in wordToGuess){
@@ -173,14 +163,17 @@ function displayLetterBox(){
 }
 
 
-
 function countDownTimer() {
-    document.getElementById('countDown').innerHTML = "Du har " + (timeLeft < 10 ? "0" : "") + String(timeLeft + "s kvar");
+    document.getElementById('countDown').innerHTML = 
+    `Du har ${timeLeft < 10 ? "0" : ""}${timeLeft}s kvar`;
     timeLeft--;
+    console.log(stopTimer);
     if (timeLeft < 0) {
         playerLose();
     }
     else {
-        setTimeout(countDownTimer, 1000);
+        if(!stopTimer) {
+            setTimeout(countDownTimer, 1000);
+        }
     }
 }
